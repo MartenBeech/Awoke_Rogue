d582e315cd311adc3f3b36032a50b798e3e3339b
MartenBeech <mortenbech22@hotmail.com>
2020-10-01 10:45:01 +0200

    Tiles added

---
+++ Awoke Rogue/Assets/RoomGenerator.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RoomGenerator : MonoBehaviour
+{
+    
+    public void GenerateRoom()
+    {
+        FillWithWall();
+
+        AddImageToTiles();
+    }
+
+    private void FillWithWall()
+    {
+        for (int i = 0; i < Tile.Tiles.Length; i++)
+        {
+            Tile.type[i] = "Wall";
+        }
+    }
+
+    private void AddPath(int xStart, int xEnd, int yStart, int yEnd)
+    {
+
+    }
+
+    private void AddImageToTiles()
+    {
+        for (int i = 0; i < Tile.Tiles.Length; i++)
+        {
+            if (Tile.type[i] == "Wall")
+            {
+                Tile.Tiles[i].GetComponentInChildren<Image>().sprite = Resources.Load<Sprite>("Walls/Wall");
+            }
+        }
+        
+    }
+}
---
+++ Awoke Rogue/Assets/Tile.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Tile : MonoBehaviour
+{
+    const int SIZE = 1600;
+    public static GameObject[] Tiles = new GameObject[SIZE];
+
+    public static string[] type = new string[SIZE];
+
+    private void Start()
+    {
+        for (int i = 0; i < 40; i++)
+        {
+            for (int j = 0; j < 40; j++)
+            {
+                Tiles[j + (40 * i)] = GameObject.Find("TilesHorizontal (" + i + ")/Tile (" + j + ")");
+            }
+        }
+
+        RoomGenerator room = new RoomGenerator();
+        room.GenerateRoom();
+    }
+}
